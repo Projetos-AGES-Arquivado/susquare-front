f5434d38bd1207ef357d48fdc89f599d2fe57e58
Luis Filipe Campani <luisfilipecampani@gmail.com>
2016-10-15 13:08:52 -0300

    Missing files o.o

--- SUSquare/Controller/HealthUnitViewController.swift
+++ SUSquare/Controller/HealthUnitViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import MapKit
 import CoreLocation
 import SVProgressHUD
 
-class HealthUnitViewController: UIViewController, CLLocationManagerDelegate {
+class HealthUnitViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: MKMapView!
     
+    var searchBar : CustomSearchBar?
+    
+    var shouldShowSearchResults : Bool = false
     var healthUnits = [HealthUnit]()
     
+    var filteredHealthUnits = [HealthUnit]()
+    
     var locationManager : CLLocationManager!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
@@ -25,6 +32,7 @@ class HealthUnitViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
+        loadUnits()
         if let location = locationManager.location{
             SVProgressHUD.show(withStatus: "Loading HealthUnits")
             let coordinate : CLLocationCoordinate2D = location.coordinate
@@ -46,12 +54,114 @@ class HealthUnitViewController: UIViewController, CLLocationManagerDelegate {
         }
         locationManager.stopUpdatingLocation()
         
+        
+        self.configureSearchBar(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 50), font: UIFont(name: "Verdana", size: 16.0)!, textColor: UIColor.white, bgColor: UIColor(red: 71, green: 186, blue: 251))
+        
+        tableView.tableHeaderView = self.searchBar
+        self.searchBar?.delegate = self
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        checkLocationAuthorizationStatus()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func loadUnits() {
+        SVProgressHUD.show(withStatus: "Loading HealthUnits")
+        let coordinate: CLLocationCoordinate2D = (locationManager.location?.coordinate)!
+        RestManager.sharedInstance.requestHealthUnits(byLocation: coordinate, withRange: 10, withBlock: { (units: [HealthUnit]?, error: Error?) in
+            if error == nil {
+                for unit in units! {
+                    let a = HealthUnitMapAnnotation(healthUnit: unit)
+                    self.mapView.addAnnotation(a)
+                    self.healthUnits.append(unit)
+                    //self.tableView.reloadData()
+                }
+                SVProgressHUD.dismiss()
+            } else {
+                print(error)
+                SVProgressHUD.showError(withStatus: error?.localizedDescription)
+            }
+        })
+        centerMap()
+    }
+    
+    func centerMap() {
+        if let location = locationManager.location {
+            centerMapOnLocation(location: location)
+        }
+    }
+    
+    //MARK: SearchBar
+    func configureSearchBar(frame: CGRect, font: UIFont, textColor: UIColor, bgColor: UIColor) {
+        searchBar = CustomSearchBar(frame: frame, font: font , textColor: textColor)
+        
+        searchBar?.barTintColor = bgColor
+        searchBar?.tintColor = textColor
+        searchBar?.showsBookmarkButton = false
+        searchBar?.showsCancelButton = true
+//        searchBar?.setImage(UIImage(named: "StarFlat"), for: .search, state: .normal)
+        
+        searchBar?.placeholder = "Buscar Posto de Saúde"
+        searchBar?.setScopeBarButtonBackgroundImage(UIImage(named: "StarFlat"), for: .normal)
+        
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filterHealthUnitsForSearchText(searchText)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        self.filterHealthUnitsForSearchText("")
+        self.view.endEditing(true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if !shouldShowSearchResults {
+            shouldShowSearchResults = true
+            tableView.reloadData()
+        }
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    func filterHealthUnitsForSearchText(_ searchText: String, scope: String = "All") {
+        self.filteredHealthUnits = healthUnits.filter { healthUnits in
+            print(healthUnits.unitName?.lowercased())
+            print(searchText.lowercased())
+            return (healthUnits.unitName?.lowercased().contains(searchText.lowercased()))!
+        }
+        
+        if self.filteredHealthUnits.isEmpty {
+            shouldShowSearchResults = false
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    // MARK: MapView Helpers
+    //location manager to authorize user location for Maps app
+    func checkLocationAuthorizationStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let regionRadius: CLLocationDistance = 10000
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -69,9 +179,14 @@ class HealthUnitViewController: UIViewController, CLLocationManagerDelegate {
         let userLocation = CLLocation(latitude: (User.sharedInstance.location?.latitude)!, longitude: (User.sharedInstance.location?.longitude)!)
         let unitLocation = CLLocation(latitude: healthUnitLocation.latitude, longitude: healthUnitLocation.longitude)
         
-        let distanceInMeters : CLLocationDistance = userLocation.distance(from: unitLocation)
+        let distanceInMeters = Int((userLocation.distance(from: unitLocation)))
         
-        return "\(distanceInMeters)km"
+        let distanceInKilometers = distanceInMeters/1000
+        
+        print("IN M: \(distanceInMeters) ----- IN KM:\(distanceInKilometers)")
+        
+        
+        return "\(distanceInKilometers)km"
     }
 }
 
@@ -84,17 +199,75 @@ extension HealthUnitViewController : UITableViewDelegate {
 
 extension HealthUnitViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return healthUnits.count
+        if shouldShowSearchResults{
+            return filteredHealthUnits.count
+        } else {
+            return healthUnits.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "healthUnitIdentifier", for: indexPath) as! HealthUnitTableViewCell
         
-        let healthUnit : HealthUnit = healthUnits[indexPath.row]
+        let healthUnit : HealthUnit
         
+        if shouldShowSearchResults{
+            healthUnit = healthUnits[indexPath.row]
+        }else {
+            healthUnit = healthUnits[indexPath.row]
+        }
         cell.lblDistance.text = self.calcDistanceToHealthUnit(healthUnitLocation: healthUnit.location!)
         cell.lblHealthUnit.text = healthUnit.unitName
         
         return cell
+    }
+}
+
+//MARK: MKMapViewDelegate
+extension HealthUnitViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? HealthUnitMapAnnotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+                as? MKPinAnnotationView { // 2
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                // 3
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let annotation = view.annotation as? HealthUnitMapAnnotation {
+            if let hu = annotation.healthUnit {
+                performSegue(withIdentifier: "healthUnitDetails", sender: hu)
+            }
+        }
+    }
+}
+
+//MARK: CLLocationManagerDelegate
+extension HealthUnitViewController: CLLocationManagerDelegate {
+    private func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let location = locations.last as! CLLocation
+        
+        User.sharedInstance.location = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region = MKCoordinateRegion(center: User.sharedInstance.location!, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        mapView.setRegion(region, animated: true)
+    }
+}
+
+extension HealthUnitViewController : UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController){
+        filterHealthUnitsForSearchText(searchController.searchBar.text!)
     }
 }
--- SUSquare/Model/CustomSearchBar.swift
+++ SUSquare/Model/CustomSearchBar.swift
@@ -7,3 +7,65 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomSearchBar: UISearchBar {
+    var preferredFont: UIFont!
+    
+    var preferredTextColor: UIColor!
+    
+    init(frame: CGRect, font: UIFont, textColor: UIColor) {
+        super.init(frame: frame)
+        
+        self.frame = frame
+        preferredFont = font
+        preferredTextColor = textColor
+        setShowsCancelButton(false, animated: false)
+        searchBarStyle = .prominent
+        isTranslucent = false
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    func indexOfSearchFieldInSubviews() -> Int! {
+        var index: Int!
+        let searchBarView = subviews[0]
+        
+        for i in 0 ..< searchBarView.subviews.count {
+            if searchBarView.subviews[i] is UITextField {
+                index = i
+                break
+            }
+        }
+        
+        return index
+    }
+    
+    override func draw(_ rect: CGRect) {
+        // Find the index of the search field in the search bar subviews.
+        if let index = indexOfSearchFieldInSubviews() {
+            // Access the search field
+            let searchField: UITextField = (subviews[0]).subviews[index] as! UITextField
+            
+            searchField.attributedPlaceholder = NSAttributedString(string: "Buscar Posto de Saúde", attributes: [NSForegroundColorAttributeName:UIColor.white])
+            
+            UIButton.appearance(whenContainedInInstancesOf: [CustomSearchBar.self]).tintColor = .white
+            
+//            UIButton.appearanceWhenContainedInInstancesOfClasses().tintColor  = .white
+            
+            // Set its frame.
+            searchField.frame = CGRect(x: 5.0, y: 5.0, width: frame.size.width - 60.0, height: frame.size.height - 10.0)
+            
+            // Set the font and text color of the search field.
+            searchField.font = preferredFont
+            searchField.textColor = preferredTextColor
+            
+            // Set the background color of the search field.
+            searchField.backgroundColor = barTintColor
+        }
+        
+        super.draw(rect)
+    }
+}
